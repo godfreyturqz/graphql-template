69f471537fae6200d483dd1335e831c498bee67e
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-01-03 21:48:09 +0800

    refactoring

--- CLIENT/src/components/Post.js
+++ CLIENT/src/components/Post.js
@@ -1,48 +1,25 @@
 import React from 'react'
 // LOGIC
 import Post__Get from './Post__Get'
-import Post__Create from './Post__Create'
-import Post__Update from './Post__Update'
-import Post__Delete from './Post__Delete'
 // COMPONENTS
-import PostEdit from './PostEdit'
+import PostElement from './PostElement'
+import Loading from './Loading'
+import Error from './Error'
+import PostCreate from './PostCreate'
 
 
 const Post = () => {
 
     const { loading, data, error } = Post__Get()
-    const { postValue, handleTextArea, handleSubmitPost } = Post__Create()
-    const { handleEdit, isModalOpen, setIsModalOpen, updateValue, handleInputUpdate, handleSubmitUpdate } = Post__Update()
-    const { handleDelete } = Post__Delete()
-
 
     return (
         <div>
-            <h1>What's on your mind?</h1>
-            <form onSubmit={handleSubmitPost}>
-                <textarea cols="30" rows="5" onChange={handleTextArea} value={postValue}/>
-                <button>Post</button>
-            </form>
-            <hr/>
+            <PostCreate />
             <h3>Posts</h3>
             {
-                loading ? <div>Loading...</div>:
-                error ? <div>Error! {error.message}</div> :
-                data?.posts.map(({id, post}) => 
-                    <div key={id}>
-                        <br/>
-                        <div>{post}</div>
-                        <button onClick={() => handleEdit(id)}>Edit</button>
-                        <button onClick={() => handleDelete(id)}>Delete</button>
-                        <PostEdit 
-                            isModalOpen = {isModalOpen}
-                            setIsModalOpen = {setIsModalOpen}
-                            updateValue = {updateValue}
-                            handleInputUpdate = {handleInputUpdate}
-                            handleSubmitUpdate = {handleSubmitUpdate}
-                        />
-                    </div>
-                )
+                loading ? <Loading /> :
+                error ? <Error error={error.message}/> :
+                data?.posts.map(props => <PostElement key={props.id} {...props}/>)
             }
         </div>
     )
